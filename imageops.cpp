3be8fe8fe4583552efcca8358fb65f87c9896861
dancanmatara <dancangwe@gmail.com>
2019-04-09 03:46:15 +0200

    Modified the imageops.h and cpp files, the .h file is fully implemented as required. For the .cpp file, I am left with the implementation of the overloaded operators and the save/load functions of the image class. In addition, I need to implement the filtering operator %

--- imageops.cpp
+++ imageops.cpp
@@ -1,55 +1,101 @@
+/*
+* Author : Dancan Angwenyi
+* Date : 8th April 2019
+* Computer Science
+* C++ Image processing (Using containers and operators)
+*/
 #include "imageops.h"
 using namespace std;
 
-namespace ANGDAN002{
-    
-    //default constructor
-    Image::Image(){}
-    //default destructor
-    Image::~Image(){}
-    //parametrized constructor
-    Image::Image(int width, int height ,std::unique_ptr<unsigned char[]> data)
-        :width(width)
-        ,height(height)
-        ,data(data.get())
-    {};
-    //copy constructor
-    Image::Image (const Image& image)
-        :width(image.width)
-        ,height(image.height)
-        ,data(image.data.get())
-    {};
-    //Move constructor
-    Image::Image (Image&& other)
-        :width(other.width)
-        ,height(other.height)
-        ,data(move(other.data.get()))
-    {};
-    Image::Image& Image::operator=(const Image& other)
-    {
-        if (this != &other) {
+namespace ANGDAN002 {
+
+//default constructor
+Image::Image()
+{
+    this->width = 0;
+    this->height = 0;
+}
+//default destructor
+Image::~Image()
+{
+    this->width = -1;
+    this->height = -1;
+    data.reset(nullptr);
+}
+//parametrized constructor
+Image::Image(int width, int height)
+    : width(width)
+    , height(height){};
+//copy constructor
+Image::Image(Image& other)
+{
+    this->width = other.width;
+    this->height = other.height;
+    this->data.reset(new unsigned char[width * height]);
+    Image::iterator beg = this->begin(), end = this->end();
+    Image::iterator inStart = other.begin(), inEnd = other.end();
+    while ( beg != end) { *beg = *inStart; ++beg; ++inStart; } 
+}
+//Move constructor
+Image::Image(Image&& other)
+    : width(other.width)
+    , height(other.height)
+{
+    this->data.reset(new unsigned char[width * height]);
+    this->data = std::move(other.data);
+}
+//copy assignment operator
+Image::Image& Image::operator=(Image& other)
+{
+    if (this != &other) {
         this->width = other.width;
         this->height = other.height;
-       //s data.getData() = other.data.get();  
-        }
-         
+        this->data.reset(new unsigned char[width * height]);
+        Image::iterator beg = this->begin(), end = this->end();
+        Image::iterator inStart = other.begin(), inEnd = other.end();
+        while ( beg != end) { *beg = *inStart; ++beg; ++inStart; } 
+    }
     return *this;
-    }
-    Image::Image& Image::operator=(Image&& other){
-        if (this != &other) {
+}
+//move assignment operator
+Image::Image& Image::operator=(Image&& other)
+{
+    if (this != &other) {
         this->width = other.width;
         this->height = other.height;
-        //this->getData() = move(other.data.get());
-        other.setWidth(0);
-        other.setHeight(0);
-        other.setData(nullptr);
-        }
-        return *this;
-   
+        this->data.reset(new unsigned char[width * height]);
+        this->data = std::move(other.data);
+        other.width = -1;
+        other.height = -1;
     }
+    return *this;
+}
+//Accessors
+int Image::getWidth() const
+{
+    return width;
+}
+int Image::getHeight() const
+{
+    return height;
+}
+//Mutators
+void Image::setWidth(int width)
+{
+    this->width = width;
+}
+void Image::setHeight(int height)
+{
+    this->height = height;
+}
+//overloaded operators
+Image Image::operator+(Image& other){}
+Image Image::operator-(Image& other){}
+Image Image::operator!(){}
+Image Image::operator/(Image& others){}
+Image Image::operator*(int f){}
 
-
-
-
-
+//load and save images
+void Image::load(std::string inputFileName) {}
+void Image::save(std::string outputFileName) {}
 }
--- imageops.h
+++ imageops.h
@@ -1,4 +1,9 @@
-
+/*
+* Author : Dancan Angwenyi
+* Date : 8th April 2019
+* Computer Science
+* C++ Image processing (Using containers and operators)
+*/
 #ifndef IMAGEOPS_H_
 #define IMAGEOPS_H_
 #include <stdexcept>
@@ -9,6 +14,7 @@
 #include "catch.hpp"
 #include <vector>
 #include <algorithm>
+#include <memory>
 
 namespace ANGDAN002 {
 class Image {
@@ -19,26 +25,33 @@ private:
 public:
     Image(); //default constructor
     ~Image(); //default destructor
-    //Parametirized constructor
-    Image(int width, int height ,std::unique_ptr<unsigned char[]> data);
-    Image (const Image& image); //Copy constructor
-    Image (Image&& other); //Move constructor
-    Image& operator=(const Image& other); //Assignment operator
+    Image(int width, int height); //Parametirized constructor
+    Image(Image& image); //Copy constructor
+    Image(Image&& other); //Move constructor
+    Image& operator=(Image& other); //Assignment operator
     Image& operator=(Image&& other); //Move assignment operator
-    /***************************Mutators******************/
-    void setWidth(int width);
-    void setHeight(int height);
-    void setData(std::unique_ptr<unsigned char[]> data);
-    /*********************************************************/
-    /***********************Accessors*************************/
+    //Accessors
     int getWidth() const;
     int getHeight() const;
-    std::unique_ptr<unsigned char[]> getData() const;
-    /*********************************************************/
+    //Mutators
+    void setWidth(int width);
+    void setHeight(int height);
+    //overloaded operators
+    Image operator+(Image& other);
+    Image operator-(Image& other);
+    Image operator!();
+    Image operator/(Image& L1);
+    Image operator*(int i);
+    //load and save images
+    void load(std::string inputFileName);
+    void save(std::string outputFileName);
+
+    //start class iterator and make image the friend
     class iterator {
     private:
         unsigned char* ptr;
         // construct only via Image class (begin/end)
+        friend class Image;
         iterator(u_char* p)
             : ptr(p)
         {
@@ -52,14 +65,47 @@ public:
         // define overloaded ops: *, ++, --, =
         iterator& operator=(const iterator& rhs)
         {
+            if (this != &rhs) {
+                this->ptr = rhs.ptr;
+            }
+            return *this;
         }
         // other methods for iterator
+        unsigned char& operator*()
+        {
+            return *ptr;
+        }
+        iterator& operator++()
+        {
+            ++ptr;
+            return (*this);
+        }
+        iterator& operator--()
+        {
+            --ptr;
+            return (*this);
+        }
+        bool operator!=(const iterator& rhs)
+        {
+            if (this->ptr != rhs.ptr) {
+                return true;
+            }
+
+            else {
+                return false;
+            }
+        }
     };
     // define begin()/end() to get iterator to start and
-    // "one-past" end.
-    iterator begin(void) { return iterator(data.get()) } // etc
+    iterator begin(void)
+    {
+        return iterator(data.get());
+    }
+    iterator end(void)
+    {
+        return iterator(data.get() + (width * height));
+    }
 };
-}
 }
 
 #endif
